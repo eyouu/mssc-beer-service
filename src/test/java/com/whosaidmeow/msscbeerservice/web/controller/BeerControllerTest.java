35d93a6001cb57773f41094225a10571468f6a39
Vasyl Zhovnir <vasyl.zhovnir@cgm.com>
2021-11-17 21:39:59 +0200

    added controller test

---
+++ src/test/java/com/whosaidmeow/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,53 @@
+package com.whosaidmeow.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.whosaidmeow.msscbeerservice.web.model.BeerDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws Exception {
+        BeerDTO beerDTO = BeerDTO.builder().build();
+        String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
+
+        mockMvc.perform(post("/api/v1/beer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDTOJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDTO beerDTO = BeerDTO.builder().build();
+        String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
+
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDTOJson))
+                .andExpect(status().isNoContent());
+    }
+}
