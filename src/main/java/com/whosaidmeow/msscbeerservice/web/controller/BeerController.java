5f3e1db011b233cb00144c541ff3178a37cf28a0
Vasyl Zhovnir <vasyl.zhovnir@cgm.com>
2021-11-17 21:22:10 +0200

    added controller

---
+++ src/main/java/com/whosaidmeow/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,31 @@
+package com.whosaidmeow.msscbeerservice.web.controller;
+
+import com.whosaidmeow.msscbeerservice.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable UUID beerId) {
+
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beerDTO) {
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDTO beerDTO) {
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
