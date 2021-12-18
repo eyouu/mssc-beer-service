b6eeb37ccc5271fefb6cba477520ea363c750f79
Vasyl <vazhovnir@gmail.com>
2021-12-18 22:46:59 +0200

    updates

--- src/main/java/com/whosaidmeow/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/whosaidmeow/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,12 +2,14 @@ package com.whosaidmeow.msscbeerservice.bootstrap;
 
 import com.whosaidmeow.msscbeerservice.domain.Beer;
 import com.whosaidmeow.msscbeerservice.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-//@Component
+@RequiredArgsConstructor
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -15,10 +17,6 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_3_UPC = "0083783375213";
 
     private final BeerRepository beerRepository;
-
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
 
     @Override
     public void run(String... args) throws Exception {
