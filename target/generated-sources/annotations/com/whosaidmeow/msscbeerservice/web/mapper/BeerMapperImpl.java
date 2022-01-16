70c8037efad606622bec567a00a4b893fc176fe6
Vasyl <vazhovnir@gmail.com>
2022-01-16 17:51:10 +0200

    updated

---
+++ target/generated-sources/annotations/com/whosaidmeow/msscbeerservice/web/mapper/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package com.whosaidmeow.msscbeerservice.web.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-12-22T17:25:02+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
