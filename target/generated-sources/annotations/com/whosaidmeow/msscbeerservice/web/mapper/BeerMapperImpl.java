367b41a280b7032a00facff89691d2bc8b44ef00
Vasyl <vazhovnir@gmail.com>
2022-02-04 12:50:21 +0200

    updates

--- target/generated-sources/annotations/com/whosaidmeow/msscbeerservice/web/mapper/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/whosaidmeow/msscbeerservice/web/mapper/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-02-04T11:52:17+0200",
+    date = "2022-02-04T12:45:55+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
 )
 @Component
